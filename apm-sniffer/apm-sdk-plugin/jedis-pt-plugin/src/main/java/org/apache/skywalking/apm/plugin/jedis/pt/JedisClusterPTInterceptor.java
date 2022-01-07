f792016502fa8d32cffa2943626c86b726e947a3
lijian <lijian123@oppo.com>
2022-01-07 18:24:10 +0800

    * jedis pt implement

---
+++ apm-sniffer/apm-sdk-plugin/jedis-pt-plugin/src/main/java/org/apache/skywalking/apm/plugin/jedis/pt/JedisClusterPTInterceptor.java
@@ -0,0 +1,47 @@
+package org.apache.skywalking.apm.plugin.jedis.pt;
+
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
+import org.apache.skywalking.apm.plugin.jedis.pt.common.JedisTool;
+import org.apache.skywalking.apm.plugin.pt.commons.PressureTestContext;
+import org.apache.skywalking.apm.plugin.pt.commons.enums.CacheShadowMode;
+
+import java.lang.reflect.Method;
+
+import static org.apache.skywalking.apm.plugin.jedis.pt.JedisPTPluginConfig.Plugin.JedisPT.CACHE_SHADOW_MODE;
+
+/**
+ * @author lijian
+ * @since 2022/1/7
+ */
+public class JedisClusterPTInterceptor implements InstanceMethodsAroundInterceptor {
+
+	@Override
+	public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, MethodInterceptResult result) throws Throwable {
+		if (PressureTestContext.isTest()) {
+			if (CACHE_SHADOW_MODE == CacheShadowMode.SHADOW_DB) {
+				JedisTool.InvokeResp invokeResp = JedisTool.executeByShadowDB(method, allArguments, argumentsTypes);
+				if (invokeResp.isSuccess()) {
+					result.defineReturnValue(invokeResp.getResponse());
+				}
+			} else if (CACHE_SHADOW_MODE == CacheShadowMode.SHADOW_KEY) {
+				// judge the arguments, to convert the key to shadow key
+				JedisClusterMethodStrategy strategy = JedisClusterMethodStrategy.getStrategy(method.getName(), argumentsTypes);
+				if (strategy != null) {
+					strategy.doArguments(allArguments);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Object ret) throws Throwable {
+		return ret;
+	}
+
+	@Override
+	public void handleMethodException(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Throwable t) {
+
+	}
+}
